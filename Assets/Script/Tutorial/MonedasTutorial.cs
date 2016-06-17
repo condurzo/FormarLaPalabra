ab109b790bf1ace3c1579829be0ec983e0bae3b1
Maximiliano <maximiliano.condurzo@gmail.com>
2016-06-17 16:12:59 -0300

    Error Fixs

---
+++ Assets/Script/Tutorial/MonedasTutorial.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonedasTutorial : MonoBehaviour {
+
+	public int Monedas;
+	public TextMesh MonedasText;
+
+	void Update () {
+		Monedas = PlayerPrefs.GetInt("Monedas");
+		MonedasText.text = Monedas.ToString();
+	}
+}
